e4b01893c21880298dbad4caaca0b3ca2523c270
MaxTBrand <max@tomorrowbrand.de>
2025-02-27 19:11:02 +0100

    Add files via upload

---
+++ druckstation_nextjs_railway_procfile/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const next = require("next");
+
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`🚀 Server läuft auf Port ${port}`);
+  });
+});
